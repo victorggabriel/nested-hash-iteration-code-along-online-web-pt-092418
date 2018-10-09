561a4a5d7de568d58f72df783303b4138f7944c4
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-09 05:25:32 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,8 +19,10 @@ def remove_strawberry(contacts)
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
           data.delete_if {|ice_cream| ice_cream == "strawberry"}
-        binding.pry
-      end  
+        
+      end
     end
   end
 end
+
+remove_strawberry(contacts)
